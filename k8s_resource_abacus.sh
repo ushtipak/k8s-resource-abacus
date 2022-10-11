d4207a5b9f37669118a90b7577a8a2b0f1b6c5eb
Milos Knezevic <ushtipak@gmail.com>
2022-10-11 18:00:26 +0200

    Fix numerous shellcheck issues :))

--- k8s_resource_abacus.sh
+++ k8s_resource_abacus.sh
@@ -6,15 +6,14 @@ SCRAPE_INTERVAL="5s"
 
 [[ ! -d metrics ]] && mkdir metrics
 
-cd metrics
+cd metrics || exit
 i=0
 start=$(date +"%T")
 
 echo "=> Metric scrape initiated [!]"
 echo "=> Interrupt with ANY key when satisfied with data gather, hopefully hours (more the marrier :))"
-while [ true ]; do
-  read -t 3 -n 1
-  if [ $? = 0 ] ; then
+while true; do
+  if read -r -t 3 -n 1; then
     break
   else
     ((i=i+1))
@@ -27,13 +26,13 @@ done
 echo " <= Received, breaking [!]"
 
 echo "=> Processing metrics ..."
-METRICS_TOTAL=$(grep -v 'Metrics gather' "${DEPLOYMENT}" | wc -l)
-P90=$(echo $(( METRICS_TOTAL*90/100 )))
-P99=$(echo $(( METRICS_TOTAL*99/100 )))
-CPU_P90=$(grep -v 'Metrics gather' "${DEPLOYMENT}" | sort -nk 2 | tail -n+${P90} | head -1 | awk '{print $2}')
-CPU_P99=$(grep -v 'Metrics gather' "${DEPLOYMENT}" | sort -nk 2 | tail -n+${P99} | head -1 | awk '{print $2}')
-MEMORY_P90=$(grep -v 'Metrics gather' "${DEPLOYMENT}" | sort -nk 3 | tail -n+${P90} | head -1 | awk '{print $3}')
-MEMORY_P99=$(grep -v 'Metrics gather' "${DEPLOYMENT}" | sort -nk 3 | tail -n+${P99} | head -1 | awk '{print $3}')
+METRICS_TOTAL=$(grep -v 'Metrics gather' "${DEPLOYMENT}" -c)
+P90=$(( METRICS_TOTAL*90/100 ))
+P99=$(( METRICS_TOTAL*99/100 ))
+CPU_P90=$(grep -v 'Metrics gather' "${DEPLOYMENT}" | sort -nk 2 | tail -n"+${P90}" | head -1 | awk '{print $2}')
+CPU_P99=$(grep -v 'Metrics gather' "${DEPLOYMENT}" | sort -nk 2 | tail -n"+${P99}" | head -1 | awk '{print $2}')
+MEMORY_P90=$(grep -v 'Metrics gather' "${DEPLOYMENT}" | sort -nk 3 | tail -n"+${P90}" | head -1 | awk '{print $3}')
+MEMORY_P99=$(grep -v 'Metrics gather' "${DEPLOYMENT}" | sort -nk 3 | tail -n"+${P99}" | head -1 | awk '{print $3}')
 
 echo
 echo "=> Stats of ${DEPLOYMENT} deployment"
